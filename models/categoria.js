026964e19f1fa36ef4cfaa9c4872721f369aa5f1
paul pichon <paul10_barca@hotmail.com>
2023-02-21 15:45:58 -0600

    Modelo Categoria, creacion del Schema: ademas se creo archivo INDEX.JS en la carpeta modelos

---
+++ models/categoria.js
@@ -0,0 +1,35 @@
+//mongoose
+const { Schema, model } = require('mongoose');
+
+//funcion
+const CategoriaSchema = Schema({
+    nombre: {
+        type: String,
+        required : [ true, 'El nombre es ogligatorio']
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+        required : true //Aunque venga por defecto:true, debemos mandar el valor para que no genere un error
+    },
+    //para crear una categoria yo necesito saber que  usuario fue el que la creo
+    //este es el type que se debe poner para saber que viene de MONGO
+    //y la referencia para saber a que collecion o a que Object.Id dirigirse
+    //en este caso ref: 'Usuario', Usuario ---> viene de usuario.js nombre del Schema
+    usuario: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario', //nombre del otro Schema de MONGO
+        required: true
+    }
+});
+
+
+//exportar
+
+//se debe poner en singular ya que mongo le agrega la "S" al final de cada nombre por ejemplo
+//casa = casa"s"
+//Usuario = Usuario"s"
+//Categori = Categori"s" --> nombre de la tabla en MONGO
+//ademas debe inicar con letra mayuscula
+//y por ultimo pasamos como segundo argumento nuestro Schema en este caso es CategoriaSchema
+module.exports = model( 'Categoria', CategoriaSchema );
---
+++ models/index.js
@@ -0,0 +1,15 @@
+
+//MODELOS
+const Categoria = required('./categoria');
+const Role = required('./role');
+const Server = required('./server');
+const Usuario = required('./usuario');
+
+
+//EXPORTAR
+module.exports = {
+    Categoria,
+    Role,
+    Server,
+    Usuario
+}
