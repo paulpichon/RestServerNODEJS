ca859dd3f628ee8870810c15c96cab5ac3289c64
paul pichon <paul10_barca@hotmail.com>
2023-02-22 23:31:20 -0600

    Buscar en otras colecciones, Categorias y Productos

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -8,7 +8,7 @@ const { ObjectId } = require('mongoose').Types;
 //coleccionar permitidas
 //aqui iran todas las colecciones que se pueden usar para hacer las busquedas
 const coleccionesPermitidas = [
-    'categoria',
+    'categorias',
     'usuarios',
     'productos',
     'roles'
@@ -53,6 +53,76 @@ const buscarUsuarios = async( termino = '', res = response ) => {
 
 }
 
+//termino de busqueda ---> lo que se quiere encontrar
+//podemos iniciar el termino como un string vacio
+const buscarCategorias = async( termino = '', res = response ) => {
+
+    //verificar si algo es un ID de mongo
+    const esMongoID = ObjectId.isValid( termino ); //true o false
+
+    //si es un ID de mongo
+    if ( esMongoID ) {
+        //busquemos coincidencias del ID contra el termino
+        const categoria = await Categoria.findById( termino );
+        //respuesta
+        return res.json({
+            //preguntamos si viene la categoria si viene regresamos los resultados sino regresamos un arreglo vacio ya que no viene informacion
+            results: ( categoria ) ? [ categoria ] : []
+        });
+    }
+
+    //vamos a crear una expresion regular para poder hacer que nuestra busqueda sea key sensitive, es decir que se pueda buscar con minisculas o mayusculas y aun asi encontrar coincidencias
+    // 'i' ----> esto hace que sea insensible a mayusculas y minusculas
+    const regex = new RegExp( termino, 'i');
+
+    //buscar por nombre de usuario
+    //mandamos el regex para que no haya problema si viene en mayusculas o minusculas
+    //find() retorna un arreglo ya sea vacio o con informacion si es que encontro algo
+    //buscamos por nombre y que el estado este en TRUE
+    const categorias = await Categoria.find({nombre: regex, estado: true});
+
+    res.json({
+        results: categorias
+    });
+
+}
+
+//termino de busqueda ---> lo que se quiere encontrar
+//podemos iniciar el termino como un string vacio
+const buscarProductos = async( termino = '', res = response ) => {
+
+    //verificar si algo es un ID de mongo
+    const esMongoID = ObjectId.isValid( termino ); //true o false
+
+    //si es un ID de mongo
+    if ( esMongoID ) {
+        //busquemos coincidencias del ID contra el termino
+        const producto = await Producto.findById( termino )
+                                        .populate('categoria', 'nombre');
+        //respuesta
+        return res.json({
+            //preguntamos si viene el producto si viene regresamos los resultados sino regresamos un arreglo vacio ya que no viene informacion
+            results: ( producto ) ? [ producto ] : []
+        });
+    }
+
+    //vamos a crear una expresion regular para poder hacer que nuestra busqueda sea key sensitive, es decir que se pueda buscar con minisculas o mayusculas y aun asi encontrar coincidencias
+    // 'i' ----> esto hace que sea insensible a mayusculas y minusculas
+    const regex = new RegExp( termino, 'i');
+
+    //buscar por nombre de usuario
+    //mandamos el regex para que no haya problema si viene en mayusculas o minusculas
+    //find() retorna un arreglo ya sea vacio o con informacion si es que encontro algo
+    //buscamos por nombre y que el estado este en TRUE
+    const productos = await Producto.find({nombre: regex, estado: true})
+                                    .populate('categoria', 'nombre');
+
+    res.json({
+        results: productos
+    });
+
+}
+
 const buscar = ( req, res = response) => {
 
     //extraemos la coleccion y el termino de busqueda
@@ -70,14 +140,16 @@ const buscar = ( req, res = response) => {
     //switch
     switch ( coleccion ) {
         case 'usuarios':
-            //mandamos la funcion buscarUsuarios
+            //mandamos la funcion buscarUsuarios junsto con la res: response
             buscarUsuarios( termino, res );
         break;
-        case 'categoria':
-            
+        case 'categorias':
+            //mandamos la funcion buscarCategorias junsto con la res: response
+            buscarCategorias( termino, res );
         break;
         case 'productos':
-            
+            //mandamos la funcion buscarProductos junto con la res: response
+            buscarProductos( termino, res );
         break;
         //por defectoponemos un error 500 indicando que es un error nuestro
         default:
