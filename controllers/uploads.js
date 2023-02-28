fb643b90b88e152d3d1b729c8e00c471f2eb37e7
Paul Pichon F <paul10_barca@hotmail.com>
2023-02-27 18:48:51 -0600

    Validar la extension del archivo a subir

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -20,20 +20,47 @@ const cargarArchivo = ( req, res = response ) => {
     //desestructuramos
     //establecer lo que viene en el require.files()
     const { archivo } = req.files;
+    //hacer split al nombre del archivo a subir
+    const nombreCortado = archivo.name.split('.');
+    
+    //extension del archivo
+    //la extension va a ser igual a la ultima posicion del arreglo nombreCortado
+    const extension = nombreCortado[ nombreCortado.length - 1 ];
+
+    //validar la extencion
+    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
+
+    //validar si empata la extension que subimos con la del arreglo extensionesValidas
+    //si no hay coincidencias
+    if ( !extensionesValidas.includes( extension )  ) {
+        //mensaje de error
+        return res.status( 400 ).json({
+            msg: `La extension ${ extension } no es permitida, ${ extensionesValidas } `
+        });
+    }
+
+
+    res.json({
+        extension
+    });
+
+    console.log( nombreCortado );
+
+
     //construccion del path a donde moveremos el archivo
     //The join() method creates and returns a new string by concatenating all of the elements in an array
-    const uploadPath = path.join( __dirname, '../uploads/', archivo.name );
+    // const uploadPath = path.join( __dirname, '../uploads/', archivo.name );
 
-    //.mv() ---> mueve el archivo
-    archivo.mv(uploadPath, (err) => {
-        if (err) {
-        return res.status(500).json({err});
-        }
+    // //.mv() ---> mueve el archivo
+    // archivo.mv(uploadPath, (err) => {
+    //     if (err) {
+    //     return res.status(500).json({err});
+    //     }
 
-        res.json({
-            msg: 'File uploaded to ' + uploadPath
-        });
-    });
+    //     res.json({
+    //         msg: 'File uploaded to ' + uploadPath
+    //     });
+    // });
 
 
 }
